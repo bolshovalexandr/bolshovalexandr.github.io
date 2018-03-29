cca02ca8bdeee4167f17b45727b4989826218cbe
Bolshov Alexandr <bolshov@inbox.ru>
2018-03-29 21:47:50 +0300

    heroku

--- precedent/js/script.js
+++ precedent/js/script.js
@@ -46,32 +46,26 @@
 
 	'use strict';
 	
-	// import auth from './tools/storage.js';
-	
 	var feedbackResponse = document.querySelector('#feedback-response');
-	var feedbackSend = document.querySelector('#feedback-send');
-	
 	var feedbackForm = document.forms.feedback;
 	
-	feedbackSend.addEventListener('click', function (evt) {
+	feedbackForm.addEventListener('submit', function (evt) {
 	  evt.preventDefault();
 	
 	  feedbackResponse.innerHTML = 'отправляем...';
 	
 	  var xhr = new XMLHttpRequest();
+	  var data = new FormData(feedbackForm);
 	
-	  xhr.open('POST', 'https://precedent.herokuapp.com/');
-	  xhr.setRequestHeader('content-type', 'application/json');
-	  var data = JSON.stringify({
-	    name: feedbackForm.name.value,
-	    message: feedbackForm.message.value,
-	    contact: feedbackForm.contact.value,
-	    employee: feedbackForm.employee.value
-	  });
+	  xhr.open();
+	  // xhr.open('POST', 'https://precedent.herokuapp.com/');
+	  // xhr.open('POST', 'http://localhost:5000');
+	
 	  xhr.onreadystatechange = function () {
 	    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
 	      var answer = JSON.parse(xhr.responseText);
-	      feedbackResponse.innerHTML = '\n\n        \u0423\u0432\u0430\u0436\u0430\u0435\u043C\u044B\u0439 <b>' + answer.name + '</b>,<br>\n        \u0412\u0430\u0448 \u0437\u0430\u043F\u0440\u043E\u0441 \u043F\u043E \u0442\u0435\u043C\u0435 \u043F\u0440\u0438\u043D\u044F\u0442 <b>' + answer.message + '</b> \u0438 \u043E\u0442\u043F\u0440\u0430\u0432\u043B\u0435\u043D \u043D\u0430 \u043F\u043E\u0447\u0442\u0443<br>\n        \u0441\u043F\u0435\u0446\u0438\u0430\u043B\u0438\u0441\u0442\u0443 <b>' + answer.employee + '</b>,<br>\n        \u043A\u043E\u0442\u043E\u0440\u044B\u0439 \u0441\u0432\u044F\u0436\u0435\u0442\u0441\u044F \u0441 \u0412\u0430\u043C\u0438 \u043F\u043E <b>' + answer.contact + '</b><br>\n      ';
+	
+	      feedbackResponse.innerHTML = '\n        \u0423\u0432\u0430\u0436\u0430\u0435\u043C\u044B\u0439 <b>' + answer.name + '</b>,<br>\n        \u0412\u0430\u0448 \u0437\u0430\u043F\u0440\u043E\u0441 \u043F\u043E \u0442\u0435\u043C\u0435 \u043F\u0440\u0438\u043D\u044F\u0442 <b>' + answer.message + '</b> \u0438 \u043E\u0442\u043F\u0440\u0430\u0432\u043B\u0435\u043D \u043D\u0430 \u043F\u043E\u0447\u0442\u0443<br>\n        \u0441\u043F\u0435\u0446\u0438\u0430\u043B\u0438\u0441\u0442\u0443 <b>' + answer.employee + '</b>,<br>\n        \u043A\u043E\u0442\u043E\u0440\u044B\u0439 \u0441\u0432\u044F\u0436\u0435\u0442\u0441\u044F \u0441 \u0412\u0430\u043C\u0438 \u043F\u043E <b>' + answer.contact + '</b><br>\n      ';
 	    }
 	  };
 	  xhr.send(data);
