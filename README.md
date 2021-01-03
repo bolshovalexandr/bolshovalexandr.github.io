cb42359dba35075fb226ced7b57689e87483e39f
Bolshov Alexander <bolshov@inbox.ru>
2021-01-04 00:05:08 +0300

    clean

--- README.md
+++
@@ -1,1 +0,0 @@
-# bolshovalexandr.github.io
--- avi/js/constants.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-  window.constants = {
-    URL: `https://server-rdzfcrscdl.now.sh/products`,
-    XHR_TIMEOUT: 10000,
-    SUCCESS_STATUS: 200,
-  };
-})();
